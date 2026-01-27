b525fb741b17c5fbeb7d847046d5e229efa902aa
aka-sidd <pandeysiddhartha1515@gmail.com>
2026-01-28 00:16:26 +0530

    Add User-defined data type example in Java

---
+++ Java Programs/src/OOPsJAVA/UserDefinedDataType.java
@@ -0,0 +1,39 @@
+package OOPsJAVA;
+import java.util.*;
+ // create own datatype
+public class UserDefinedDataType {
+    public static class Student { // create own datatype
+        String name;
+        int rno;
+        double cgpa;
+
+    }
+    public static void main(String[] args) {
+        //User input using scanner class making its object from scanner class
+        Scanner sc = new Scanner(System.in);
+       Student s1 = new Student(); // declaration
+        s1.name = "Siddharth";
+        s1.rno = 11863;
+        s1.cgpa = 7.0;
+
+        Student s2 = new Student(); // declaration
+        s2.name = "Rachna";
+        s2.rno = sc.nextInt();
+        s2.cgpa = 8.3;
+
+        Student s3 = new Student(); // declaration
+        s3.name = "Rahul";
+        s3.rno = 43;
+        s3.cgpa = 7.8;
+
+        System.out.println(s1.name + " " +s1.cgpa + " " +s1.rno);
+        s1.cgpa = 8.1; // update cgpa
+        System.out.println(s1.cgpa);
+
+        // if I want then take A input from user(Using Scanner)
+        System.out.println("Roll number of s2 is: "+ s2.rno);
+
+
+
+    }
+}
