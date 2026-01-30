9066b7423b31f9b8a70b37e7ace185e9ddfeeb8e
aka-sidd <pandeysiddhartha1515@gmail.com>
2026-01-30 19:23:59 +0530

    Implement palindrome checker with brute force, StringBuilder, and two-pointer methods

---
+++ Java Programs/src/StringPlusHashMap/CheckPalindrome.java
@@ -0,0 +1,41 @@
+package StringPlusHashMap;
+//This is Brute force
+public class CheckPalindrome {
+    public static boolean isPalindromeBrute(String s){
+        int n = s.length();
+        String rev = "";
+        for(int i=n-1;i>=0;i--){
+            rev+=s.charAt(i);
+        }
+        return s.equals(rev);
+    }
+    //This is a Better Approach
+    public static boolean isPalindromeBetter(String s){
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        for(int i=n-1;i>=0;i--){
+            sb.append(s.charAt(i));
+        }
+        return s.equals(sb.toString());
+    }
+    public static boolean isPalindromeOptimal(String s){
+        int n = s.length();
+        int left = 0,right = s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "madam";
+        System.out.println("brute: "+ isPalindromeBrute(s));
+        System.out.println("Better: " +isPalindromeBetter(s));
+        System.out.println("Optimal: " +isPalindromeOptimal(s));
+
+
+    }
+}
